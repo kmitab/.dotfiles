5a9b684564b807208a564b7e7731c9032ffd6830
bkmita <boguslaw.kmita@hkl.eu>
2021-12-27 17:17:14 +0100

    changig dir check

--- stow.sh
+++ stow.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 cd ~/.dotfiles
+if [ $? -ne 0 ]; then
+    echo "error changing directory"
+    exit 1
+fi
+
 for file in *; do
     if [ -d "$file" ]; then
         stow --verbose=2 $file
