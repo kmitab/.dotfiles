44086387ff89f06e4e131dbaf8cb75f0e5d36192
kmitab <47388254+kmitab@users.noreply.github.com>
2021-12-30 15:17:05 +0100

    Update stow.sh

--- stow.sh
+++ stow.sh
@@ -1,21 +1,21 @@
-#!/bin/bash                                            
-                                                       
-cd ~/.dotfiles                                         
-if [ $? -ne 0 ]; then                                  
-    echo "error changing directory"                    
-    exit 1                                             
-fi                                                     
-                                                       
-git fetch origin                                       
+#!/bin/bash
+
+cd ~/.dotfiles
+if [ $? -ne 0 ]; then
+    echo "error changing directory"
+    exit 1
+fi
+
+git fetch origin
 update_check="$(git log HEAD..origin/master --oneline)"
-if [ "$update_check" != "" ]; then                     
-    git reset --hard origin/master                       
-fi                                                     
-                                                       
-for file in *; do                                      
-    if [ -d "$file" ]; then                            
-        stow --verbose=2 $file                         
-    fi                                                 
-done                                                   
-                                                       
-source ~/.bashrc 
+if [ "$update_check" != "" ]; then
+    git reset --hard origin/master
+fi
+
+for file in *; do
+    if [ -d "$file" ]; then
+        stow --verbose=2 $file
+    fi
+done
+
+source ~/.bashrc
